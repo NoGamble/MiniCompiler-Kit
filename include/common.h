6fb76442ea63b708a54f714ee0519fe244069a0c
NoGamble <uestc520@gmail.com>
2025-10-10 23:50:37 +0800

    Oct10

---
+++ include/common.h
@@ -0,0 +1,13 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+void error(const char *msg);
+void warning(const char *msg);
+
+#endif
---
+++ include/lexer.h
@@ -0,0 +1,25 @@
+#ifndef LEXER_H
+#define LEXER_H
+
+#include "token.h"
+#include "common.h"
+
+typedef struct
+{
+    FILE *sourceFile;
+    FILE *outputFile;
+    FILE *errorFile;
+    int currentLine;
+    int currentChar;
+    int lookhead;
+
+} LexerState;
+
+LexerState* lexerInit(const char *source_path, const char *output_path);
+void lexerCleanup(LexerState *state);
+Token getNextToken(LexerState *state);
+void runLexer(const char *sourcePath, const char *outputPath);
+
+Token getKeywordType(const char *lexme);
+
+#endif
